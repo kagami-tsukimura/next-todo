094e9d11622b337c8d9d26d4fd2901ee51835114
Kagami <shimura.kyohei7617@gmail.com>
2023-10-25 20:41:31 +0900

    update: get tasks

--- src/server/trpc/router/todo.ts
+++ src/server/trpc/router/todo.ts
@@ -1,1 +1,30 @@
+import { createTaskSchema } from "./../../../schema/todo";
+import { authedProcedure, t } from "./../trpc";
 
+export const todoRouter = t.router({
+  createTask: authedProcedure
+    .input(createTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session?.user?.id,
+            },
+          },
+        },
+      });
+      return task;
+    }),
+  getTasks: t.procedure.query(({ ctx }) => {
+    return ctx.prisma.task.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }),
+});
