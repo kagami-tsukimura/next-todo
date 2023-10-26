48ef0d4ac0041f4ca85c5dd681620806b2e2b122
Kagami <shimura.kyohei7617@gmail.com>
2023-10-26 21:27:53 +0900

    fix: body validate at least 5

--- src/schema/todo.ts
+++ src/schema/todo.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const createTaskSchema = z.object({
   title: z.string().max(20),
-  body: z.string().max(5),
+  body: z.string().min(5),
 });
 
 export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
@@ -10,7 +10,7 @@ export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
 export const updateTaskSchema = z.object({
   taskId: z.string().cuid(),
   title: z.string().max(20),
-  body: z.string().max(5),
+  body: z.string().min(5),
 });
 
 export type UpdateTaskInput = z.TypeOf<typeof updateTaskSchema>;
